3c1433d5f7cd1c5b1abd063a02edb1fb9c30853a
Joseph Long <github@joseph-long.com>
2017-08-21 15:46:47 -0700

    Twiddle the gitconfig for credential helpers

--- .git-credential-helper.sh
+++ .git-credential-helper.sh
@@ -2,6 +2,6 @@
 UBUNTU_GIT_CREDENTIAL_PATH="/usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring"
 if [ -f $UBUNTU_GIT_CREDENTIAL_PATH ]; then
     $UBUNTU_GIT_CREDENTIAL_PATH $@
-elif [ $(command -v git-credential-osxkeychain )]
+elif [ $(command -v git-credential-osxkeychain )]; then
     git-credential-osxkeychain $@
 fi
