3f3784f51f2e522ffc7f590225e0a8e1ef77e030
Joseph Long <github@joseph-long.com>
2017-10-04 00:13:41 -0700

    Shove some customizations into darwin-configuration.nix

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,10 +1,13 @@
 { config, lib, pkgs, ... }:
+let
+  hostName = "Anansi";
+in
 {
   # Borrowed from https://github.com/LnL7/nix-darwin/blob/master/modules/examples/lnl.nix
   #
   system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
   system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
-  system.defaults.NSGlobalDomain.InitialKeyRepeat = 10;
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
   system.defaults.NSGlobalDomain.KeyRepeat = 1;
   system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
   system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
@@ -25,6 +28,42 @@
   #
   # end
 
+  # Use list view in all Finder windows by default
+  system.defaults.finder.FXPreferredViewStyle = "Nlsv";
+  # Disable Resume system-wide
+  system.defaults.NSGlobalDomain.NSQuitAlwaysKeepsWindows = false;
+  # Save to disk (not to iCloud) by default
+  system.defaults.NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+  # Disable automatic termination of inactive apps
+  system.defaults.NSGlobalDomain.NSDisableAutomaticTermination = false;
+  # Add a context menu item for showing the Web Inspector in web views
+  system.defaults.NSGlobalDomain.WebKitDeveloperExtras = true;
+  # Some settings cannot be controlled through Nix (yet)
+  system.activationScripts.extraActivation.text = ''
+    # Make NetBIOS name (Windows file sharing) consistent with networking.hostName
+    defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string ${hostName}
+    # Disable local Time Machine snapshots
+    tmutil disablelocal
+    # Put a lower limit on Bluetooth headphone audio quality
+    defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 40
+    # Disable the “Are you sure you want to open this application?” dialog
+    defaults write com.apple.LaunchServices LSQuarantine -bool false
+    # -- Messages.app --
+    # Disable smart quotes as it’s annoying for messages that contain code
+    defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+    # Disable automatic spelling corrections
+    defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticSpellingCorrectionEnabled" -bool false
+    # -- Activity Monitor --
+    # Show the main window when launching Activity Monitor
+    defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+    # Visualize CPU usage in the Activity Monitor Dock icon
+    defaults write com.apple.ActivityMonitor IconType -int 5
+    # Show all processes in Activity Monitor
+    defaults write com.apple.ActivityMonitor ShowCategory -int 0
+    # Sort Activity Monitor results by CPU usage
+    defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+    defaults write com.apple.ActivityMonitor SortDirection -int 0
+  '';
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
@@ -32,13 +71,13 @@
       pkgs.git
       pkgs.htop
       pkgs.mosh
+      pkgs.texlive.combined.scheme-full
     ];
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.bash.enable = true;
 
   # Configure hostname
   networking.hostName = "Anansi";
-
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.bash.enable = true;
 
   # Recreate /run/current-system symlink after boot.
   services.activate-system.enable = true;
