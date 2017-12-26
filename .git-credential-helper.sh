4decd3ed5cc2dedc771753013380a15a4c4cfb53
Joseph Long <github@joseph-long.com>
2017-12-26 15:54:45 -0500

    Fall back to storing git credentials in ~/.git-credentials (safe enough on non-shared systems...)

--- .git-credential-helper.sh
+++ .git-credential-helper.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
+set -e
 UBUNTU_GIT_CREDENTIAL_PATH="/usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring"
 if [ -f $UBUNTU_GIT_CREDENTIAL_PATH ]; then
     $UBUNTU_GIT_CREDENTIAL_PATH $@
 elif [ $(command -v git-credential-osxkeychain) ]; then
     git-credential-osxkeychain $@
 else
-    echo "No git credential helpers found..."
-    exit 1
+    git credential-store $@
 fi
