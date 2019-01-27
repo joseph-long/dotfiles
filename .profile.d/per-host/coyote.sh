4fad2666896a3bd0dd2e1d4bfff5b3e619d43ed8
Joseph Long <github@joseph-long.com>
2019-01-27 13:25:35 -0700

    Add rust / cargo

--- .profile.d/per-host/coyote.sh
+++ .profile.d/per-host/coyote.sh
@@ -1,2 +1,5 @@
 export CLICOLOR=1
 alias grep='grep --color'
+export PATH="/Users/jlong/miniconda3/bin:$PATH"
+export MXMAKEFILE="/Users/jlong/devel/mxlib/mk/MxApp.mk"
+export LD_LIBRARY_PATH="/Users/jlong/.local/lib:/opt/intel/mkl/lib:$LD_LIBRARY_PATH"
