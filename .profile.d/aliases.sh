7cdbd2a58d72643a7f9007e0fefc3edc8e691e82
Joseph Long <github@joseph-long.com>
2019-04-30 16:37:30 -0700

    Add Exercism C++ alias

--- .profile.d/aliases.sh
+++ .profile.d/aliases.sh
@@ -1,1 +1,14 @@
 alias shrinkpdf="gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=shrunken.pdf"
+alias ds9="ds9 -multiframe -mode region -zoom to fit"
+
+function excpp() {
+    exercism download --exercise=$1 --track=cpp
+    exdir="$HOME/Exercism/cpp/$1"
+    cd $exdir
+    touch $1.{cpp,h}
+    mkdir -p build
+    cd build
+    cmake -G "Unix Makefiles" ..
+    make
+    code ..
+}
