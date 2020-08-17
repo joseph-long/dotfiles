8b847e44730ba9228070f5d3980c48ccd398476a
Joseph Long <github@joseph-long.com>
2020-08-17 12:02:41 -0700

    update hpc dotfiles

--- .profile.d/per-host/uahpc.sh
+++ .profile.d/per-host/uahpc.sh
@@ -1,4 +1,8 @@
-module load singularity
+if [[ $(command -v module) == "module" ]]; then
+    module load singularity
+    module load intel/mkl/64
+fi
 export PATH="/groups/jrmales/josephlong/.local/bin:$PATH"
 export LD_LIBRARY_PATH="/groups/jrmales/josephlong/.local/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/groups/jrmales/josephlong/.local/lib/pkgconfig:$PKG_CONFIG_PATH"
+alias myq="qstat -w -n -u josephlong"
