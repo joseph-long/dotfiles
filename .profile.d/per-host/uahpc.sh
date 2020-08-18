a63bd4fb8c9a8265612ea027ace7f9c5ce509326
Joseph Long <github@joseph-long.com>
2020-08-17 22:40:02 -0700

    New hpc aliases

--- .profile.d/per-host/uahpc.sh
+++ .profile.d/per-host/uahpc.sh
@@ -5,4 +5,15 @@ fi
 export PATH="/groups/jrmales/josephlong/.local/bin:$PATH"
 export LD_LIBRARY_PATH="/groups/jrmales/josephlong/.local/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/groups/jrmales/josephlong/.local/lib/pkgconfig:$PKG_CONFIG_PATH"
-alias myq="qstat -w -n -u josephlong"
+if command -v srun &> /dev/null; then
+    export _UAHPC_SYS=SLURM
+elif command -v qstat &> /dev/null; then
+    export _UAHPC_SYS=PBS
+fi
+function myq() {
+    if [[ $_UAHPC_SYS == PBS ]]; then
+        qstat -w -n -u $USER
+    elif [[ $_UAHPC_SYS == SLURM ]]; then
+        squeue -u $USER
+    fi
+}
--- .profile.d/ua-hpc.sh
+++ .profile.d/ua-hpc.sh
@@ -1,4 +1,5 @@
-if [[ $(hostname -f) != "*cluster" ]]; then
+if [[ $(hostname -f) != *cluster && $(hostname -f) != *hpc.arizona.edu ]]; then
     alias ocelote="ssh -J josephlong@hpc.arizona.edu josephlong@login.ocelote.hpc.arizona.edu"
     alias elgato="ssh -J josephlong@hpc.arizona.edu josephlong@login.elgato.hpc.arizona.edu"
+    alias puma="ssh -J josephlong@hpc.arizona.edu josephlong@shell.hpc.arizona.edu"
 fi
