60b524d2f853434d40b6c07167abc5c0d2bb838c
Joseph Long <github@joseph-long.com>
2020-08-22 15:01:23 -0700

    Add puma, corona, magaox new roles for prefix prompt

--- .profile.d/magao-x.sh
+++ .profile.d/magao-x.sh
@@ -6,6 +6,10 @@ if [[ ! -z $MAGAOX_ROLE ]]; then
         bgcolor=42
     elif [[ $MAGAOX_ROLE == ICC ]]; then
         bgcolor=46
+    elif [[ $MAGAOX_ROLE == TIC ]]; then
+        bgcolor=43
+    elif [[ $MAGAOX_ROLE == TOC ]]; then
+        bgcolor=44
     else
         bgcolor=47
     fi
