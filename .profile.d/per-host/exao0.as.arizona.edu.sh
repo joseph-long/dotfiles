8beac75f179bb8445cdc2e4dd51781cad2d2a2d4
Joseph Long <me@joseph-long.com>
2020-11-10 14:06:12 -0700

    Add exao0 profile customizations

--- .profile.d/per-host/exao0.as.arizona.edu.sh
+++ .profile.d/per-host/exao0.as.arizona.edu.sh
@@ -1,10 +1,24 @@
-PATH=$PATH:$HOME/.local/bin:$HOME/bin
 
+PATH=$HOME/anaconda3/bin:$PATH:$HOME/.local/bin:$HOME/bin
+eval "$(/home/jlong/anaconda3/bin/conda shell.bash hook)"
+# PATH=$PATH:$HOME/.local/bin:$HOME/bin
 export PATH
+unset CONDA_SHLVL
+unset CONDA_PROMPT_MODIFIER
+unset CONDA_EXE
+unset _CE_CONDA
+unset CONDA_PREFIX_1
+unset CONDA_MKL_INTERFACE_LAYER_BACKUP
+unset CONDA_PREFIX
+unset CONDA_PYTHON_EXE
+unset CONDA_DEFAULT_ENV
+unset CONDA_PREFIX
+unset conda
 export LD_LIBRARY_PATH="$HOME/.local/lib:/opt/intel/mkl/lib/intel64:/opt/intel/compilers_and_libraries_2017.4.196/linux/tbb/lib/intel64_lin/gcc4.7:/opt/intel/compilers_and_libraries_2017.4.196/linux/compiler/lib/intel64_lin:/opt/intel/compilers_and_libraries_2017.4.196/linux/mkl/lib/intel64_lin::/usr/local/cuda-8.0/lib"
 export MXMAKEFILE="/home/jlong/devel/mxlib/mk/MxApp.mk"
 export LD_LIBRARY_PATH="/home/jlong/.local/lib:/opt/intel/mkl/lib/intel64:/opt/intel/compilers_and_libraries_2017.4.196/linux/tbb/lib/intel64_lin/gcc4.7:/opt/intel/compilers_and_libraries_2017.4.196/linux/compiler/lib/intel64_lin:/opt/intel/compilers_and_libraries_2017.4.196/linux/mkl/lib/intel64_lin::/usr/local/cuda-8.0/lib"
 export LD_LIBRARY_PATH="/home/jlong/.local/lib:/opt/intel/mkl/lib/intel64:/opt/rh/devtoolset-7/root/usr/lib64:/opt/rh/devtoolset-7/root/usr/lib:/opt/rh/devtoolset-7/root/usr/lib64/dyninst:/opt/rh/devtoolset-7/root/usr/lib/dyninst:/opt/rh/devtoolset-7/root/usr/lib64:/opt/rh/devtoolset-7/root/usr/lib:/opt/rh/devtoolset-7/root/usr/lib64:/opt/rh/devtoolset-7/root/usr/lib:/opt/rh/devtoolset-7/root/usr/lib64/dyninst:/opt/rh/devtoolset-7/root/usr/lib/dyninst:/opt/rh/devtoolset-7/root/usr/lib64:/opt/rh/devtoolset-7/root/usr/lib:/opt/intel/compilers_and_libraries_2017.4.196/linux/tbb/lib/intel64_lin/gcc4.7:/opt/intel/compilers_and_libraries_2017.4.196/linux/compiler/lib/intel64_lin:/opt/intel/compilers_and_libraries_2017.4.196/linux/mkl/lib/intel64_lin::/usr/local/cuda-8.0/lib"
 . /opt/rh/devtoolset-7/enable
 unset PYTHONPATH
-export PATH="/home/jlong/miniconda3/bin:$PATH"
+source /opt/intel/vtune_profiler_2020.2.0.610396/vtune-vars.sh
+export MPLBACKEND=Agg
